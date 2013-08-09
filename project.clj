fd094ae1f20df6bc3791612048af0b8b493c5223
guns <self@sungpae.com>
2013-08-09 17:49:58 -0500

    v1.0.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject guns.cli/optparse "1.0.0"
+(defproject guns.cli/optparse "1.0.1"
   :url "https://github.com/guns/optparse-clj"
-  :description "OptionParser for Clojure. Works like clojure.tools.cli, but supports GNU option parsing conventions."
+  :description "OptionParser for Clojure: Functional GNU-style command line options parsing."
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :aliases {"example" ["trampoline" "run" "-m" "example"]}
