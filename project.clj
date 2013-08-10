04ad342461b2299efdb80786d90910fd63f0353e
guns <self@sungpae.com>
2013-08-10 07:10:39 -0500

    Change tagline

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject guns.cli/optparse "1.0.1"
   :url "https://github.com/guns/optparse-clj"
-  :description "OptionParser for Clojure: Functional GNU-style command line options parsing."
+  :description "OptionParser for Clojure: Functional GNU-style command line options parser."
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :aliases {"example" ["trampoline" "run" "-m" "example"]}
